8ab52191468f8a13aa265e3f327d1751b7827192
Josedavid Ruiz <JosedavidRH30@gmail.com>
2025-08-11 21:24:11 -0500

    feat: move index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import ReactDOM from "react-dom/client"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import Index from './pages/Index'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-  root.render(
-    <h1>Hola</h1>
-  )
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Index />
+  </StrictMode>,
+)
 
