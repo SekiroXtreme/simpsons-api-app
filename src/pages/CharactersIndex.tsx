04063c82d1b8475cadedcde9d6a0009a417c97d6
Josedavid Ruiz <JosedavidRH30@gmail.com>
2025-08-30 13:44:15 -0500

    feat: refactor charactersindex show

--- src/pages/CharactersIndex.tsx
+++ src/pages/CharactersIndex.tsx
@@ -4,12 +4,24 @@ import CardItem from "../components/layout/Card";
 import CardGrid from "../components/layout/CardGrid";
 import { useCharacters } from "../hooks/useCharacter";
 import Modal from "../components/layout/Modal";
-import type { Character } from "../types";
+import type { Character, Characters } from "../types";
+import axios from "axios";
 
 export default function CharacterIndex() {
   const { characters, loading, page, hasMore, loadNext, loadPrev } = useCharacters();
   const [selectedChar, setSelectedChar] = useState<Character | null>(null);
+  const [loadingChar, setLoadingChar] = useState(false);
   const navigate = useNavigate();
+
+  const fetchCharacter = async (id: number) => {
+    try {
+      setLoadingChar(true);
+      const { data } = await axios.get<Character>(`https://thesimpsonsapi.com/api/characters/${id}`);
+      setSelectedChar(data);
+    } finally {
+      setLoadingChar(false);
+    }
+  };
 
   if (loading) return <p className="text-center mt-4">Loading...</p>;
 
@@ -22,10 +34,10 @@ export default function CharacterIndex() {
         loadNext={loadNext}
         loadPrev={loadPrev}
       >
-        {characters.map((char) => (
+        {characters.map((char: Characters) => (
           <CardItem key={char.id}>
             <div
-              onClick={() => setSelectedChar(char as Character)}
+              onClick={() => fetchCharacter(char.id)}
               className="cursor-pointer p-4"
             >
               <div className="flex justify-center p-4">
@@ -46,42 +58,46 @@ export default function CharacterIndex() {
       </CardGrid>
 
       <Modal open={!!selectedChar} onClose={() => setSelectedChar(null)}>
-        {selectedChar && (
-          <div className="p-4 flex flex-col gap-3 text-center justify-center items-center bg-white w-[70vw] lg:w-[40vw] rounded-md">
-            <img
-              src={`https://cdn.thesimpsonsapi.com/500${selectedChar.portrait_path}`}
-              alt={selectedChar.name}
-              className="w-[15vh] h-full object-cover rounded-full border-4 border-yellow-400 shadow-md"
-            />
-            <h2 className="text-xl font-bold mb-4">{selectedChar.name}</h2>
-            <div className="flex flex-col gap-2">
-              <p className="text-gray-700">{selectedChar.description || "No description available"}</p>
-              <p className="font-bold">Phrases</p>
-              {selectedChar.phrases?.length ? (
-                <div className="mt-4 flex flex-wrap gap-2 justify-start">
-                  {selectedChar.phrases.slice(0, 3).map((phrase, i) => (
-                    <span
-                      key={i}
-                      className="bg-yellow-400 text-black px-3 py-1 rounded-lg font-medium text-sm"
-                    >
-                      {phrase}
-                    </span>
-                  ))}
-                </div>
-              ) : (
-                <p className="text-gray-500 italic mt-2">No phrases available</p>
-              )}
+        {loadingChar ? (
+          <p className="text-center p-4">Loading character...</p>
+        ) : (
+          selectedChar && (
+            <div className="p-4 flex flex-col gap-3 text-center justify-center items-center bg-white w-[70vw] lg:w-[40vw] rounded-md">
+              <img
+                src={`https://cdn.thesimpsonsapi.com/500${selectedChar.portrait_path}`}
+                alt={selectedChar.name}
+                className="w-[15vh] h-full object-cover rounded-full border-4 border-yellow-400 shadow-md"
+              />
+              <h2 className="text-xl font-bold mb-4">{selectedChar.name}</h2>
+              <div className="flex flex-col gap-2">
+                <p className="text-gray-700">{selectedChar.description || "No description available"}</p>
+                <p className="font-bold">Phrases</p>
+                {selectedChar.phrases?.length ? (
+                  <div className="mt-4 flex flex-wrap gap-2 justify-start">
+                    {selectedChar.phrases.slice(0, 3).map((phrase, i) => (
+                      <span
+                        key={i}
+                        className="bg-yellow-400 text-black px-3 py-1 rounded-lg font-medium text-sm"
+                      >
+                        {phrase}
+                      </span>
+                    ))}
+                  </div>
+                ) : (
+                  <p className="text-gray-500 italic mt-2">No phrases available</p>
+                )}
+              </div>
+              <button
+                onClick={() => {
+                  navigate(`/cardshow/${selectedChar.id}`);
+                  setSelectedChar(null);
+                }}
+                className="mt-4 px-4 py-2 bg-yellow-400 text-black font-bold rounded-lg hover:bg-yellow-500"
+              >
+                See more →
+              </button>
             </div>
-            <button
-              onClick={() => {
-                navigate(`/cardshow/${selectedChar.id}`);
-                setSelectedChar(null);
-              }}
-              className="mt-4 px-4 py-2 bg-yellow-400 text-black font-bold rounded-lg hover:bg-yellow-500"
-            >
-              See more →
-            </button>
-          </div>
+          )
         )}
       </Modal>
     </>
